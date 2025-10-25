48620ef38802d8ab30efbca5d5ea5b599014416a
Alejandro Campbell <alejandro.campbell@live.u-tad.com>
2025-10-25 16:34:32 +0200

    Portfolio generator algorythm and projects data

---
+++ assets/js/portfolio-builder.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    //const container = document.getElementById("portfolio-grid");
+    const grid = document.querySelector("#portfolio-grid");
+
+    //if (!container) return;
+    if (!grid) return;
+
+    projects.forEach(project => {
+        const item = document.createElement("div");
+        item.className = `col-md-4 col-sm-6 col-xs-12 item-space portfolio-content portfolio-item isotope-item ${project.category}`;
+        item.innerHTML = `
+          <figure >
+            <img src="${project.image}" alt="${project.title}">
+            <div class="portfolio-info">
+              <h4>${project.year}</h4>
+              <figcaption>
+                <h3>${project.title}</h3>
+                <span>${project.roles}</span>
+                <a href="${project.link}" target="_blank" rel="noopener noreferrer" class="button">Take a look</a>
+              </figcaption>
+            </div>
+          </figure>
+        `;
+        grid.appendChild(item);
+        
+        //container.insertAdjacentHTML("beforeend", html);
+    });
+
+    // 🔹 Reinicializa Isotope después de añadir los elementos
+    const iso = new Isotope(grid, {
+        itemSelector: ".portfolio-item",
+        layoutMode: "masonry"
+    });
+
+    // Esperar a que las imágenes carguen antes de hacer el layout
+    imagesLoaded(grid, () => {
+        iso.layout();
+    });
+
+
+    // 🔹 Configura los filtros
+    const filters = document.querySelectorAll("#portfolio-filters li");
+    filters.forEach(btn => {
+        btn.addEventListener("click", () => {
+            filters.forEach(b => b.classList.remove("filter-active"));
+            btn.classList.add("filter-active");
+
+            const filterValue = btn.getAttribute("data-filter");
+            iso.arrange({filter: filterValue});
+        });
+    });
+
+
+
+    // 🔹 Reinicia Glightbox (para que reconozca los nuevos elementos)
+    GLightbox({ selector: ".glightbox" });
+
+
+
+});
---
+++ assets/js/portfolio-data.js
@@ -0,0 +1,109 @@
+/*
+const projects =
+    [
+        {
+            title:,
+            year:,
+            roles:,
+            image:,
+            link:
+        },
+    ];
+
+*/
+
+const projects =
+    [
+        {
+            title: "Hermetica",
+            year: 2024,
+            roles: "Game Designer",
+            description: "Hermetica: A deckbuilder roguelike",
+            image:"assets/img/portfolio/HERMETICA-KEYART.png" ,
+            link: "projectsPages/project-Hermetica.html",
+            category: "filter-game",
+        },
+
+        {
+            title: "Lady Umbrella",
+            year: 2025,
+            roles: "Game Designer & Level Designer",
+            description: "Hermetica: A deckbuilder roguelike",
+            image:"assets/img/portfolio/LadyUmbrella_Cover_1200x1200.png" ,
+            link: "projectsPages/project-LadyUmbrella.html",
+            category: "filter-game",
+        },
+
+        {
+            title: "Alche Mice",
+            year: 2024,
+            roles: "Game Designer",
+            description: "Alche Mice: A deckbuilder roguelike",
+            image: "assets/img/portfolio/AlcheMice_Cover.png",
+            link: "projectsPages/project-AlcheMice.html",
+            category: "filter-game",
+        },
+        {
+            title: "Moonlit Mystery",
+            year: 2023,
+            roles: "Game Designer",
+            description: "Moonlit Mystery: A puzzle-detective video game",
+            image: "assets/img/portfolio/MoonlitMystery_Cover.png",
+            link: "https://alexvbfpn.itch.io/moonlit-mystery",
+            category: "filter-jam",
+        },
+        {
+            title: "Lullaby",
+            year: 2023,
+            roles: "Game Designer",
+            description: "Lullaby: A 3d platformer videogame",
+            image: "assets/img/portfolio/Lullaby_CoverNight.jpg",
+            link: "https://origamingcreations.itch.io/lullaby",
+            category: "filter-jam",
+        },
+        {
+            title: "Doing Time",
+            year: 2023,
+            roles: "Game Designer",
+            description: "Doing Time: A puzzle heist video game",
+            image: "assets/img/portfolio/DoingTime_Cover_Horizontal.png",
+            link: "https://alexvbfpn.itch.io/doing-time",
+            category: "filter-jam",
+        },
+        {
+            title: "Once Button A Time (OBAT)",
+            year: 2024,
+            roles: "Game Designer",
+            description: "OBAT: A 2.5D platformer game",
+            image: "assets/img/portfolio/OnceButtonATime_Cover.png",
+            link: "https://origamingcreations.itch.io/once-button-a-time",
+            category: "filter-jam",
+        },
+        {
+            title: "Inseye Out",
+            year: 2024,
+            roles: "Game Designer",
+            description: "Inseye Out: A procedural tool for conveying emotions through eye expression",
+            image: "assets/img/portfolio/InseyeOut_Cover.png",
+            link: "https://alexvbfpn.itch.io/inseye-out",
+            category: "filter-tool",
+        },
+        {
+            title: "Stellar Maze",
+            year: 2025,
+            roles: "Game Designer",
+            description: "Stellar Maze: A 3D puzzle game around constellations",
+            image: "assets/img/portfolio/StellarMaze_Cover.png",
+            link: "https://alexvbfpn.itch.io/stellar-maze",
+            category: "filter-jam",
+        },
+        {
+            title: "Recreating Game Mechanics",
+            year: 2024,
+            roles: "Game Designer",
+            description: "TFG project about videogame mechanics recreation from games like: God of War, Metroid Dread, Superliminal or Viewfinder",
+            image: "assets/img/portfolio/RecreatingMechanics_Cover.png",
+            link: "https://alexvbfpn.itch.io/recreating-game-mechanics",
+            category: "filter-other",
+        },
+    ];
