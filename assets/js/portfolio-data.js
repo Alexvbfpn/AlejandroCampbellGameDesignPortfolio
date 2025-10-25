a00caca1833450172d740704b2fb1f54655be1cf
Alejandro Campbell <alejandro.campbell@live.u-tad.com>
2025-10-25 17:04:02 +0200

    Role info updated

--- assets/js/portfolio-data.js
+++ assets/js/portfolio-data.js
@@ -28,7 +28,7 @@ const projects =
         {
             title: "Lady Umbrella",
             year: 2025,
-            roles: "Game Designer & Level Designer",
+            roles: "Game Designer | Level Designer ",
             description: "Hermetica: A deckbuilder roguelike",
             image:"assets/img/portfolio/LadyUmbrella_Cover_1200x1200.png" ,
             link: "projectsPages/project-LadyUmbrella.html",
@@ -39,7 +39,7 @@ const projects =
         {
             title: "Alche Mice",
             year: 2024,
-            roles: "Game Designer",
+            roles: "Technical Designer | Game Programmer",
             description: "Alche Mice: A deckbuilder roguelike",
             image: "assets/img/portfolio/AlcheMice_Cover.png",
             link: "projectsPages/project-AlcheMice.html",
@@ -49,7 +49,7 @@ const projects =
         {
             title: "Moonlit Mystery",
             year: 2023,
-            roles: "Game Designer",
+            roles: "Game Designer | Game Programmer",
             description: "Moonlit Mystery: A puzzle-detective video game",
             image: "assets/img/portfolio/MoonlitMystery_Cover.png",
             link: "https://alexvbfpn.itch.io/moonlit-mystery",
@@ -59,7 +59,7 @@ const projects =
         {
             title: "Lullaby",
             year: 2023,
-            roles: "Game Designer",
+            roles: "Game Designer | Game Programmer | Producer",
             description: "Lullaby: A 3d platformer videogame",
             image: "assets/img/portfolio/Lullaby_CoverNight.jpg",
             link: "https://origamingcreations.itch.io/lullaby",
@@ -69,7 +69,7 @@ const projects =
         {
             title: "Doing Time",
             year: 2023,
-            roles: "Game Designer",
+            roles: "Game Designer | Game Programmer | Producer",
             description: "Doing Time: A puzzle heist video game",
             image: "assets/img/portfolio/DoingTime_Cover_Horizontal.png",
             link: "https://alexvbfpn.itch.io/doing-time",
@@ -79,7 +79,7 @@ const projects =
         {
             title: "Once Button A Time (OBAT)",
             year: 2024,
-            roles: "Game Designer",
+            roles: "Game Designer | Game Programmer | Producer",
             description: "OBAT: A 2.5D platformer game",
             image: "assets/img/portfolio/OnceButtonATime_Cover.png",
             link: "https://origamingcreations.itch.io/once-button-a-time",
@@ -89,7 +89,7 @@ const projects =
         {
             title: "Inseye Out",
             year: 2024,
-            roles: "Game Designer",
+            roles: "Technical Designer | Game Programmer",
             description: "Inseye Out: A procedural tool for conveying emotions through eye expression",
             image: "assets/img/portfolio/InseyeOut_Cover.png",
             link: "https://alexvbfpn.itch.io/inseye-out",
@@ -99,7 +99,7 @@ const projects =
         {
             title: "Stellar Maze",
             year: 2025,
-            roles: "Game Designer",
+            roles: "Game Designer | Level Designer | Game Programmer",
             description: "Stellar Maze: A 3D puzzle game around constellations",
             image: "assets/img/portfolio/StellarMaze_Cover.png",
             link: "https://alexvbfpn.itch.io/stellar-maze",
@@ -109,7 +109,7 @@ const projects =
         {
             title: "Recreating Game Mechanics",
             year: 2024,
-            roles: "Game Designer",
+            roles: "Technical Designer | Game Programmer",
             description: "TFG project about videogame mechanics recreation from games like: God of War, Metroid Dread, Superliminal or Viewfinder",
             image: "assets/img/portfolio/RecreatingMechanics_Cover.png",
             link: "https://alexvbfpn.itch.io/recreating-game-mechanics",
